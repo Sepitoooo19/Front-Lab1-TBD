cadcbabe0bb698d5870e4a3572f894cc258f0c9d
Sepitoooo19 <benjamin.sepulveda@usach.cl>
2025-05-10 18:04:16 -0400

    intento de arreglo

--- pages/home-admin.vue
+++ pages/home-admin.vue
@@ -3,7 +3,7 @@
       <main class="p-6">
         <h2 class="text-2xl font-bold mb-4">Gestión del Sistema</h2>
         <p>Desde aquí puedes administrar clientes, productos, pedidos y repartidores.</p>
-        <!-- Contenido adicional para administración -->
+        <!-- Contenido adicional para administración hola-->
       </main>
     </div>
   </template>
